12ac573e9891018f81d5a68a730a7c1d68ae9f69
awdfaf <awdfaf@kakao.com>
2023-05-15 10:51:25 +0900

    5.15 보이스피싱 웹 적용

--- gw/nextjs-client/app/server/UseEffect.js
+++ gw/nextjs-client/app/server/UseEffect.js
@@ -5,7 +5,7 @@ import { useEffect } from "react"
 export default function UseEffect() {
 
     useEffect(() => {
-    fetch("http://127.0.0.1:5000/users").then(
+    fetch("http://127.0.0.1:5000/detection").then(
         // response 객체의 json() 이용하여 json 데이터를 객체로 변화
         res => res.json()
     ).then(
