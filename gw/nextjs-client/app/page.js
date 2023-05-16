2df725e46d921e2a27aaf0b279dbfcbd7a679c4d
awdfaf <awdfaf@kakao.com>
2023-05-16 15:56:51 +0900

    5.16 수정

--- gw/nextjs-client/app/page.js
+++ gw/nextjs-client/app/page.js
@@ -1,11 +1,29 @@
-
+import { Storage } from "@google-cloud/storage";
 
 export default async function Home() {
   
+  const {Storage} = require('@google-cloud/storage');
 
+  const storage = new Storage({
+    projectId: 'helical-button-381600',
+    keyFilename: '../helical-button-381600-33e40d727182.json' // 서비스 계정 키 파일 경로
+  });
+
+  const bucket = storage.bucket('awdfaf_storage');
+
+  const file = bucket.file('aaa.png');
+
+  file.createWriteStream()
+    .on('error', function(err) {})
+    .on('finish', function() {
+      // The file upload is complete.
+    })
+    .end('./aaa.png');
 
   return (
-      <div>안녕</div>
+      <div>
+        시작페이지
+      </div>
       
   )
 }
