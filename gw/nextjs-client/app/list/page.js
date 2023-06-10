40b8b240fef4a43999c4e191da44b99a48d58445
awdfaf <awdfaf@kakao.com>
2023-06-10 16:15:12 +0900

    6.10 시각화 작업

--- gw/nextjs-client/app/list/page.js
+++ gw/nextjs-client/app/list/page.js
@@ -8,7 +8,15 @@ export default function List(){
     return(
         <div>
             <h2>계좌 목록</h2>
+            <span className="link-container">
             <Link href="/qrreader">QR코드 리더</Link>
+            </span>
+            <span className="link-container">
+            <Link href="/consumption">소비 패턴 분석</Link>
+            </span>
+            <span className="link-container">
+            <Link href="/qrreader">이상치 탐지</Link>
+            </span>
             <hr />
             <Getitems></Getitems>
         </div>
